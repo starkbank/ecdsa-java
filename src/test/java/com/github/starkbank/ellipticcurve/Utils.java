723608d383ec40d9a997f599b0c3a0f3319b325f
taroninak <tgtaron@gmail.com>
2019-01-20 16:20:25 +0400

    Add tests for open ssl support

---
+++ src/test/java/com/github/starkbank/ellipticcurve/Utils.java
@@ -0,0 +1,21 @@
+package com.github.starkbank.ellipticcurve;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+/**
+ * Company: SFL LLC
+ * Created on 17-Jan-19
+ *
+ * @author Taron Petrosyan
+ */
+class Utils {
+
+    static String readFileAsString(String path) throws URISyntaxException, IOException {
+        return new String(Files.readAllBytes(Paths.get(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(path)).toURI())));
+
+    }
+}

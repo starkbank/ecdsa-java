a5ccd160137d49faa3766027f8463c1635e78ff8
Taron Petrosyan <taron.petrosyan@sflpro.com>
2019-01-23 17:12:16 +0400

    Use Java 6 API's for reading files instead of Java 7

--- src/test/java/com/github/starkbank/ellipticcurve/Utils.java
+++ src/test/java/com/github/starkbank/ellipticcurve/Utils.java
@@ -1,10 +1,8 @@
 package com.github.starkbank.ellipticcurve;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Objects;
 
 /**
  * Created on 17-Jan-19
@@ -14,12 +12,25 @@ import java.util.Objects;
 class Utils {
 
     static String readFileAsString(String path) throws URISyntaxException, IOException {
-        return new String(Files.readAllBytes(Paths.get(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(path)).toURI())));
-
+        return new String(readFileAsBytes(path), "ASCII");
     }
 
-    static byte[] readFileAsBytes(String path) throws URISyntaxException, IOException {
-        return Files.readAllBytes(Paths.get(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(path)).toURI()));
+    static byte[] readFileAsBytes(String path) throws URISyntaxException {
+        return read(ClassLoader.getSystemClassLoader().getResource(path).toURI().getPath());
+    }
 
+    private static byte[] read(String path) {
+        try {
+            RandomAccessFile f = new RandomAccessFile(path, "r");
+            if (f.length() > Integer.MAX_VALUE)
+                throw new RuntimeException("File is too large");
+            byte[] b = new byte[(int) f.length()];
+            f.readFully(b);
+            if (f.getFilePointer() != f.length())
+                throw new RuntimeException("File length changed while reading");
+            return b;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read file");
+        }
     }
 }
